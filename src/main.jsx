7301b576129aa3917d0179e891d451bfd6241bd9
Patrick <angelopatrickriosnolasco@gmail.com>
2023-05-21 19:39:02 -0500

    feat: modified main added basename

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,11 @@ import { store } from "./app/store";
 import { Provider } from "react-redux";
 document.body.style.fontFamily = "Roboto, sans-serif";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter basename="/web-ieee-frontend">
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
