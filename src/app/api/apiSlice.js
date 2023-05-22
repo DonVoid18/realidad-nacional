7ef4ad6483bbebdc14d9f1a7478999e73b88b118
Patrick <angelopatrickriosnolasco@gmail.com>
2023-05-21 22:38:50 -0500

    feat: implement url api ieee

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3000",
+  baseUrl: "https://api-ieee-unheval.onrender.com",
 });
 export const apiSlice = createApi({
   tagTypes: ["Member"],
