05af75dde64a1da70fbc61b405ec0aab1bd12478
Patrick Rios <angelopatrickriosnolasco@gmail.com>
2024-11-26 14:27:51 -0500

    cambio router

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
+import "typeface-roboto";
+import "./index.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import "typeface-roboto";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { store } from "./app/store";
 import { Provider } from "react-redux";
+import App from "./App";
+import { store } from "./app/store";
 document.body.style.fontFamily = "Roboto, sans-serif";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter basename="/web-ieee-frontend">
+    <BrowserRouter basename="/realidad-nacional">
       <Routes>
         <Route path="/*" element={<App />} />
       </Routes>
